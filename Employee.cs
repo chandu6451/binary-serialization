37f3e9b1c61d95b620363cf8fec74ed898a32e36
chandu6451 <43878555+chandu6451@users.noreply.github.com>
2018-10-06 14:59:20 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySerializationDemo
+{
+    [Serializable]
+    public class Employee
+    {
+        int id;
+        public int EmpID { get { return id; } set { id = value; } }
+
+        string name;
+        public string Name { get { return name; } set { name = value; } }
+
+        [NonSerialized]
+        double sal;
+        public double Salary { get { return sal; } set { sal = value; } }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BinarySerializationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee() { EmpID = 101, Name = "Robert", Salary = 30000 };
+            FileStream fs = new FileStream("binary.txt", FileMode.Create, FileAccess.Write);
+            BinaryFormatter bin = new BinaryFormatter();
+            bin.Serialize(fs, emp);
+            fs.Close();
+            Console.WriteLine("Serialization Done");
+
+            fs = new FileStream("binary.txt", FileMode.Open, FileAccess.Read);
+            Employee anotherEmp = (Employee)bin.Deserialize(fs);
+            fs.Close();
+            Console.WriteLine("Employee ID : " + anotherEmp.EmpID);
+            Console.WriteLine("Employee Name : " + anotherEmp.Name);
+            Console.WriteLine("Employee Salary : " + anotherEmp.Salary);
+
+            Console.ReadKey();
+        }
+    }
+}
